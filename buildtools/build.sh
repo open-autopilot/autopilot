b5d51e5431f713c33f910fbdfe058fc7e3aa2c63
Luuk van Berkel <lah.vanberkel@student.avans.nl>
2023-11-29 11:05:34 +0100

    Adding nav2 config, updating gitignore, autocreating builddir

--- buildtools/build.sh
+++ buildtools/build.sh
@@ -36,6 +36,10 @@ while [[ $# -gt 0 ]]; do
     shift
 done
 
+if [-z $SCRIPT_DIR/build]; then
+    mkdir $SCRIPT_DIR/build
+fi
+
 # Check if platform is null
 if [ -z "$PLATFORMS" ]; then
     echo "Error: --platform option is required."
