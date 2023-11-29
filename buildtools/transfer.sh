f1089157e05f18b9b52e4bd1e56f826fc978139a
Luuk van Berkel <lah.vanberkel@student.avans.nl>
2023-11-29 10:02:10 +0100

    Fixed bug in transfer script

--- buildtools/transfer.sh
+++ buildtools/transfer.sh
@@ -1,3 +1,6 @@
+SCRIPT_DIR="$(dirname "$0")"
+SCRIPT_DIR_FULL=$(realpath "$SCRIPT_DIR")
+
 # Parse command-line arguments
 while [[ $# -gt 0 ]]; do
     key="$1"
@@ -40,9 +43,11 @@ PLATFORM_CLEANED=$(echo "$PLATFORM" | tr '/' '-')
 
 # Transfer build to companion device
 if [[ -z "$SERVICE" ]]; then
-    TRANSFER_POWERSHELL=$(echo "scp //wsl.localhost/Ubuntu-22.04/home/berke/autopilot/buildtools/build/\*$PLATFORM_CLEANED.tar autopilot@autopilot.local:/home/autopilot/autopilot/buildtools/build/")
+    TRANSFER_POWERSHELL=$(echo "scp //wsl.localhost/Ubuntu-22.04/$SCRIPT_DIR_FULL/build/\*$PLATFORM_CLEANED.tar autopilot@autopilot.local:/home/autopilot/autopilot/buildtools/build/")
 else 
-    TRANSFER_POWERSHELL=$(echo "scp //wsl.localhost/Ubuntu-22.04/home/berke/autopilot/buildtools/build/$SERVICE.$PLATFORM_CLEANED.tar autopilot@autopilot.local:/home/autopilot/autopilot/buildtools/build/")
+    TRANSFER_POWERSHELL=$(echo "scp //wsl.localhost/Ubuntu-22.04/$SCRIPT_DIR_FULL/build/$SERVICE.$PLATFORM_CLEANED.tar autopilot@autopilot.local:/home/autopilot/autopilot/buildtools/build/")
 fi
+
+echo $TRANSFER_POWERSHELL
 
 powershell.exe -Command $TRANSFER_POWERSHELL
